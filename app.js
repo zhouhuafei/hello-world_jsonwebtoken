dab00594e7b10faa4b5e8947b7bbabc147528c5a
zhouhuafei <1123486116@qq.com>
2018-12-25 15:02:01 +0800

    token

--- app.js
+++ app.js
@@ -11,7 +11,7 @@ const httpServer = http.createServer(app);
 
 // token
 const secret = 'suibianxiexie';
-const token = jwt.sign({username: '1123486116@qq.com'}, secret, {expiresIn: 60 * 60});
+const token = jwt.sign({username: '1123486116@qq.com'}, secret, {expiresIn: 60 * 2}); // expiresIn过期时间。单位为：秒。
 
 // redis
 const configRedis = {
@@ -22,6 +22,7 @@ const configRedis = {
 const redisClient = redis.createClient(configRedis);
 redisClient.on('connect', function () {
     console.log('redis connection open to:\n', `http://${configRedis.host}:${configRedis.port}`);
+    redisClient.set(token, JSON.stringify({username: '1123486116@qq.com'}));
     const serverHttp = httpServer.listen('8090', function () {
         console.log('server connection open to:\n', `http://localhost:${serverHttp.address().port}`);
     });
